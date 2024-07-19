7dcd2449b31a4589b89e8021e306a291cb5242ac
evelyncaraujo <evyaraujoifc@gmail.com>
2024-07-19 16:52:56 -0300

    Configfinal

--- core/models/marca.py
+++ core/models/marca.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    descricao = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=True)
 
-
-
-    def __str__(self) -> str:
-        return f"{self.descricao} {self.id}".upper()
+    def __str__(self):
+        return f"{self.nome.upper()} ({self.id})"
+    
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
--- core/views/marca.py
+++ core/views/marca.py
@@ -3,5 +3,5 @@ from core.models import Marca
 from core.serializers import MarcaSerializer
 
 class MarcaViewSet(ModelViewSet):
-    queryset = Marca.objects.order_by("nacionalidade")  
+    queryset = Marca.objects.order_by("nome")  
     serializer_class = MarcaSerializer
