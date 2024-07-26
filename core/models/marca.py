9108b870729968dc484fd380d4289322dc6346f1
evelyncaraujo <evyaraujoifc@gmail.com>
2024-07-26 16:59:13 -0300

    correão de erro

--- core/models/marca.py
+++ core/models/marca.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, null=True)
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome.upper()} ({self.id})"
